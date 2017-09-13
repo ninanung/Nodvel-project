6ceef5a3b3393713de81cf00bbf2f75461b79519
SeungJe Jun <ninanung@naver.com>
2017-09-13 10:57:56 +0900

    write more 'routes.js' file

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var passport = require("passport");
 var User = require("./models/user");
+var Novel = require("./models/novel");
 
 function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
@@ -19,4 +20,93 @@ router.use(function (req, res, next) {
     res.locals.errors = req.flash("error");
     res.locals.infos = req.flash("info");
     next();
+});
+
+router.get("/", function(req, res, next){
+    User.find().sort({ createdAt: "descending" }).exec(function(err, users){
+        if(err) { return next(err) }
+        var page = req.params.page;
+        if(page == null) { page = 1 }
+        var skipSize = (page - 1) * 10;
+        var limitSize = 10;
+        var pageNum = 1;
+        Novel.count({ ended: true }, function(err, totalCount){
+            if(err) throw err;
+            pageNum = Math.ceil(totalCount / limitSize);
+            Novel.find({ ended: true }).sort({ date: -1 }).skip(skipSize).limit(limitSize).exec(function(err, Contents){
+                if(err) throw err;
+                res.render("index", { users: users, contents: Contents, pagination: pageNum});
+            });
+        });
+    });
+});
+
+router.get("/:page", function(req, res, next){
+    User.find().sort({ createdAt: "descending" }).exec(function(err, users){
+        if(err) { return next(err) }
+        var page = req.params.page;
+        if(page == null) { page = 1 }
+        var skipSize = (page - 1) * 10;
+        var limitSize = 10;
+        var pageNum = 1;
+        Novel.count({ ended: true }, function(err, totalCount){
+            if(err) throw err;
+            pageNum = Math.ceil(totalCount / limitSize);
+            Novel.find({ ended: true }).sort({ date: -1 }).skip(skipSize).limit(limitSize).exec(function(err, Contents){
+                if(err) throw err;
+                res.render("index", { users: users, contents: Contents, pagination: pageNum});
+            });
+        });
+    });
+});
+
+router.get("/login", function (req, res) {
+    res.render("login");
+});
+
+router.post("/login", passport.authenticate("login", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true
+}));
+
+router.get("/logout", function (req, res) {
+    req.logout();
+    req.session.destroy(function (err) {
+        if (err) console.log("session destroy err occured!");
+        console.log("session destroyed!");
+    });
+    res.redirect("/");
+});
+
+router.get("/signup", function (req, res) {
+    res.render("signup");
+});
+
+router.post("/signup", function (req, res, next) {
+    var username = req.body.username;
+    var password = req.body.password;
+    var confirmPassword = req.body.confirmpassword;
+    if (password === confirmPassword) {
+        User.findOne({ username: username }, function (err, user) {
+            if (err) { return next(err); }
+            if (user) {
+                req.flash("error", "User already exists.");
+                return res.redirect("/signup");
+            }
+            var newUser = new User({
+                username: username,
+                password: password
+            });
+            newUser.save(function (err) {
+                if (err) return next(err);
+                req.flash("info", "Sign up success! Please log in.");
+                return res.redirect("/");
+            });
+        });
+    }
+    else {
+        req.flash("error", "Password not confirmed.");
+        res.redirect("/signup");
+    }
 });
